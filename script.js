a25e3982115ee62656b3afa89e0771f3ddb28c94
vikas singh <pokhriyalvikas940@gmail.com>
2023-08-10 20:43:40 +0530

    stopwatch in javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+// target dom element
+
+let hours = document.getElementById('hour');
+let minutes = document.getElementById('minute')
+let seconds = document.getElementById('second')
+let startBtn = document.getElementById('start')
+let stopBtn = document.getElementById('stop')
+let resetBtn = document.getElementById('reset')
+
+
